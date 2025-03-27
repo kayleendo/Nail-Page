6f71180c91248d3bce0c8b629c09cf43eb5a964e
Kayleen Do <kayleendo8@gmail.com>
2025-03-27 16:28:02 -0400

    finished contact sheet logistics

--- googleSheet.js
+++ googleSheet.js
@@ -1,13 +1,17 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwCDEhK_c1wluaiSkPOmlFyoRGhBAuHooWB7NtA4DtHMv-Uh0Ie3ijmuAh8fQXGyC0Q/exec'
-
-const form = document.forms['contact-form']
-
-form.addEventListener('submit', e => {
-  
-  e.preventDefault()
-  
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! Form is submitted" ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+window.addEventListener("load", function() {
+  const form = document.getElementById('contact-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Thanks you! Your form was submitted sucessfully and we will be in contact with you soon.");
+      form.reset();
+    })
+    .catch(error => console.error('Error submitting your form, please try again.', error))
+  });
+});
